d64813d76d7f16495dcb21fdb4bf199b92a9446d
Poola Binu sai <149651174+Binusai@users.noreply.github.com>
2026-02-17 00:01:58 +0530

    Update InvestmentItemDto.java

--- src/main/java/com/brick/billing/controller/dto/InvestmentItemDto.java
+++ src/main/java/com/brick/billing/controller/dto/InvestmentItemDto.java
@@ -1,9 +1,11 @@
+package com.brick.billing.controller.dto;
+
 public record InvestmentItemDto(
-    String description,
-    String packageSize,
-    Double qty,
-    Double rate,
-    Double discount,
-    Double total,
-    String remarks
+        String description,
+        String packageSize,
+        Double qty,
+        Double rate,
+        Double discount,
+        Double total,
+        String remarks
 ) {}
