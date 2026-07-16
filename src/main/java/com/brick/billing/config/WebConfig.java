cee35ec7fdc728b9855f49b5febc2fc35ea804b5
Poola Binu sai <149651174+Binusai@users.noreply.github.com>
2026-07-17 00:17:26 +0530

    Update WebConfig to add HiddenHttpMethodFilter

--- src/main/java/com/brick/billing/config/WebConfig.java
+++ src/main/java/com/brick/billing/config/WebConfig.java
@@ -1,14 +1,21 @@
 package com.brick.billing.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/").setViewName("forward:/login");
+    }
+    
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 }
