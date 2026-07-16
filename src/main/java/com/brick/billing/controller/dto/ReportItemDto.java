8049dd5b40a75c40b35a7a547caabf6cc5803c5f
Poola Binu sai <149651174+Binusai@users.noreply.github.com>
2026-07-16 17:28:49 +0530

    Add uom and discount fields to ReportItemDto

--- src/main/java/com/brick/billing/controller/dto/ReportItemDto.java
+++ src/main/java/com/brick/billing/controller/dto/ReportItemDto.java
@@ -2,7 +2,9 @@ package com.brick.billing.controller.dto;
 
 public record ReportItemDto(
         String description,
+        String uom,
         Double rate,
         Double qty,
+        Double discount,
         Double total
 ) {}
